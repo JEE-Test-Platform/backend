a5268b4fd2109935d4325a42554d7cf596df43ca
sammy-1904 <samraw1904@gmail.com>
2026-04-08 21:38:50 +0530

    fixing reordering

---
+++ prisma/pending.sql
@@ -0,0 +1,3 @@
+﻿-- AlterTable
+ALTER TABLE "questions" ADD COLUMN     "negativeMarks" DOUBLE PRECISION NOT NULL DEFAULT 0;
+
--- src/index.ts
+++ src/index.ts
@@ -25,6 +25,7 @@ const allowedOrigin = process.env.FRONTEND_URL || 'http://localhost:5173';
 
 app.use(cors({
   origin: allowedOrigin,
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   credentials: true,
 }));
 
